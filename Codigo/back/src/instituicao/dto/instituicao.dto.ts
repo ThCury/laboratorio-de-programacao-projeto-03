ce50ec37c7fb187f374df2a4c49221025007aad8
lucasgiovine <lucas.giovine23@gmail.com>
2025-06-15 17:05:25 -0300

    adicionando decorators de validação em DTO instituicao

--- Codigo/back/src/instituicao/dto/instituicao.dto.ts
+++ Codigo/back/src/instituicao/dto/instituicao.dto.ts
@@ -1,21 +1,44 @@
+import { Type } from 'class-transformer';
+import { IsArray, IsNotEmpty, IsOptional, IsString, ValidateNested } from 'class-validator';
 import { AlunoDto } from 'src/aluno/dto/aluno.dto';
 import { ProfessorDto } from 'src/professor/dto/professor.dto';
 
 export class InstituicaoDto {
-    id: number;
+
+    @IsOptional()
+    id?: number;
+
+    @IsString()
+    @IsNotEmpty()
     nome: string;
+
+    @IsString()
+    @IsNotEmpty()
     endereco: string;
+
+    @IsString()
+    @IsNotEmpty()
     CNPJ: string;
-    alunos: AlunoDto[];
-    professores: ProfessorDto[];
+
+    @IsOptional()
+    @IsArray()
+    @ValidateNested({ each: true })
+    @Type(() => AlunoDto)
+    alunos?: AlunoDto[];
+
+    @IsOptional()
+    @IsArray()
+    @ValidateNested({ each: true })
+    @Type(() => ProfessorDto)
+    professores?: ProfessorDto[];
 
     constructor(
-        id: number,
         nome: string,
         endereco: string,
         CNPJ: string,
-        alunos: AlunoDto[],
-        professores: ProfessorDto[],
+        id?: number,
+        alunos?: AlunoDto[],
+        professores?: ProfessorDto[],
     ) {
         this.id = id;
         this.nome = nome;
